55a7ede2255c2d50fac09642f1e168a202244958
My2103 <114597469+My2103@users.noreply.github.com>
2024-11-25 14:24:32 +0700

    Chinese Zodiac

---
+++ ChineseZodiac.java
@@ -0,0 +1,55 @@
+//A program that prompts the user to enter a year and displays the animal for the year.
+
+import java.util.Scanner;
+
+public class ChineseZodiac {
+    public static void main(String[] args) {
+        //Create a Scanner object
+        Scanner input = new Scanner(System.in);
+
+        //Prompt the year from user
+        System.out.print("Enter the year: ");
+        int year = input.nextInt();
+
+        int Zodiac = year % 12;
+
+        switch (Zodiac) {
+            case 0:
+                System.out.println("monkey");
+                break;
+            case 1:
+                System.out.println("rooster");
+                break;
+            case 2:
+                System.out.println("dog");
+                break;
+            case 3:
+                System.out.println("pig");
+                break;
+            case 4:
+                System.out.println("rat");
+                break;
+            case 5:
+                System.out.println("ox");
+                break;
+            case 6:
+                System.out.println("tiger");
+                break;
+            case 7:
+                System.out.println("rabbit");
+                break;
+            case 8:
+                System.out.println("dragon");
+                break;
+            case 9:
+                System.out.println("snake");
+                break;
+            case 10:
+                System.out.println("horse");
+                break;
+            case 11:
+                System.out.println("sheep");
+                break;
+        }
+    }
+}
