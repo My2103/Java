3ba1d438cd9b51bca550d01bbef85d7a4a760e43
My2103 <114597469+My2103@users.noreply.github.com>
2025-03-28 22:51:32 +0700

    Finish Bubble Tea Shop

--- BubbleTeaShop.java
+++ BubbleTeaShop.java
@@ -1,43 +1,75 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-// Main class
+
 import java.util.ArrayList;
 import java.util.List;
 
+// Main class
 public class BubbleTeaShop {
     public static void main(String[] args) {
-        List<BubbleTea> orders = new ArrayList<>();
+        List<BubbleTea> bubbleTeaList = new ArrayList<>();
 
-        // Order 1: Milk Tea with Tapioca Boba and 2x Red Bean
+        // Order 1
         BubbleTea order1 = new MilkTea();
         order1 = new TapiocaBobaPearls(order1);
         order1 = new RedBean(order1);
         order1 = new RedBean(order1);
-        orders.add(order1);
+        order1 = new AloeVera(order1);
+        bubbleTeaList.add(order1);
 
-        // Order 2: Thai Milk Tea with Aloe Vera and Popping Boba
+        // Order 2
         BubbleTea order2 = new ThaiMilkTea();
         order2 = new AloeVera(order2);
         order2 = new PoppingBoba(order2);
-        orders.add(order2);
+        bubbleTeaList.add(order2);
 
-        // Order 3: Fresh Milk with Grass Jelly, Coconut Jelly, and Crystal Boba
+        // Order 3
         BubbleTea order3 = new FreshMilk();
         order3 = new JellyToppings(order3);
         order3 = new PoppingBoba(order3);
         order3 = new CrystalBoba(order3);
-        orders.add(order3);
+        bubbleTeaList.add(order3);
 
         // Print orders
         int orderNum = 1;
-        for (BubbleTea order : orders) {
-            System.out.println("Order " + orderNum + ":");
-            System.out.println("Your order is " + order.getDescription());
-            System.out.println("Total price: $" + String.format("%.2f", order.cost()));
-            System.out.println("---------------------------------------------");
-            orderNum++;
+        for (BubbleTea order : bubbleTeaList) {
+            System.out.println("\nYour order is");
+            System.out.println(order.getDescription());
+            System.out.println("-----------------------------------------------------");
+            printOrderDetails(order);
+            System.out.println("-----------------------------------------------------");
+            System.out.println(getTotalFormula(order));
         }
+    }
+
+    private static void printOrderDetails(BubbleTea bubbleTea) {
+        if (bubbleTea instanceof CondimentTea) {
+            printOrderDetails(((CondimentTea) bubbleTea).bubbleTea);
+            System.out.printf("%-13s %-13s $%.2f\n", "Topping", ((CondimentTea) bubbleTea).getToppingName(), ((CondimentTea) bubbleTea).getToppingCost());
+        } else {
+            System.out.printf("%-13s %-13s $%.2f\n", "Main", bubbleTea.getDescription(), bubbleTea.cost());
+        }
+    }
+
+    private static String getTotalFormula (BubbleTea bubbleTea) {
+        StringBuilder formula = new StringBuilder("Total = ");
+        double totalCost = 0;
+        List<Double> prices = new ArrayList<>();
+
+        while (bubbleTea instanceof CondimentTea) {
+            prices.add(((CondimentTea) bubbleTea).getToppingCost());
+            bubbleTea = ((CondimentTea) bubbleTea).bubbleTea;
+        }
+
+        prices.add(bubbleTea.cost());
+        totalCost = prices.stream().mapToDouble(Double::doubleValue).sum();
+
+        for (int i = prices.size() - 1; i >= 0; i--) {
+            formula.append(prices.get(i)).append(" + ");
+        }
+
+        return formula.substring(0, formula.length() - 3) + " = $" + String.format("%.2f", totalCost);
     }
 }
 
@@ -87,141 +119,85 @@ class FreshMilk extends BubbleTea {
 
 // Abstract decorator class
 abstract class CondimentTea extends BubbleTea {
-    public abstract String getDescription();
+    protected BubbleTea bubbleTea;
+    protected String toppingName;
+    protected double toppingCost;
+
+    public CondimentTea (BubbleTea bubbleTea, String toppingName, double toppingCost) {
+        this.bubbleTea = bubbleTea;
+        this.toppingName = toppingName;
+        this.toppingCost = toppingCost;
+    }
+
+    public String getDescription() {
+        return bubbleTea.getDescription() + ", " + toppingName;
+    }
+
+    public double cost() {
+        return bubbleTea.cost() + toppingCost;
+    }
+
+    public String getToppingName() {
+        return toppingName;
+    }
+
+    public double getToppingCost() {
+        return toppingCost;
+    }
 }
 
 // Concrete decorator: Tapioca Boba Pearls
 class TapiocaBobaPearls extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public TapiocaBobaPearls(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", Tapioca Boba Pearls";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.15;
+        super(bubbleTea, "Tapioca Pearls", 0.15);
     }
 }
 
 // Concrete decorator: Jelly Toppings
 class JellyToppings extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public JellyToppings(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", Jelly Toppings";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.20;
+        super(bubbleTea, "Jelly Toppings", 0.20);
     }
 }
 
 // Concrete decorator: Popping Boba
 class PoppingBoba extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public PoppingBoba(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", Popping Boba";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.25;
+        super(bubbleTea, "Popping Boba", 0.25);
     }
 }
 
 // Concrete decorator: Aloe Vera
 class AloeVera extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public AloeVera(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", Aloe Vera";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.30;
+        super(bubbleTea, "Aloe Vera", 0.30);
     }
 }
 
 // Concrete decorator: Red Bean
 class RedBean extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public RedBean(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", RedBean";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.30;
+        super(bubbleTea, "RedBean", 0.35);
     }
 }
 
 // Concrete decorator: Coconut Jelly
 class CoconutJelly extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public CoconutJelly(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", Coconut Jelly";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.40;
+        super(bubbleTea, "Coconut Jelly", 0.40);
     }
 }
 
 // Concrete decorator: Grass Jelly
 class GrassJelly extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public GrassJelly(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", Grass Jelly";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.45;
+        super(bubbleTea, "Grass Jelly", 0.45);
     }
 }
 
 // Concrete decorator: Crystal Boba
 class CrystalBoba extends CondimentTea {
-    BubbleTea bubbleTea;
-
     public CrystalBoba(BubbleTea bubbleTea) {
-        this.bubbleTea = bubbleTea;
-    }
-
-    public String getDescription() {
-        return bubbleTea.getDescription() + ", Crystal Boba";
-    }
-
-    public double cost() {
-        return bubbleTea.cost() + 0.50;
+        super(bubbleTea, "Crystal Boba", 0.50);
     }
 }
